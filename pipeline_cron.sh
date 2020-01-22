79de65597ff968b3560b0ef710f3fa145f60663d
josephhalstead <josephhalstead89@gmail.com>
2020-01-22 17:09:42 +0000

    change pipeline_cron for dragen

--- pipeline_cron.sh
+++ pipeline_cron.sh
@@ -14,28 +14,52 @@ function processJobs {
         #extract run info from path
         instrumentType=$(basename $(dirname "$path"))
         run=$(basename "$path")
+        
+        #if the sample sheet exists
+	if [ -f "/data/raw/$instrumentType/$run/SampleSheet.csv" ]; then
+        
+        #check whether it has Dragen in it
+	dragen=$(grep Dragen /data/raw/$instrumentType/$run/SampleSheet.csv | wc -l )
 
-        #log
-	    echo "path: $path"
-        echo "run: $run"
-        echo "instrumentType: $instrumentType"
+        else
+        
+        dragen=0
 
-        #move run to archive
-        mv "$path" /data/archive/"$instrumentType"
+        fi
 
-        #change access permissions
-        chown -R transfer /data/archive/"$instrumentType"/"$run"
-        chgrp -R transfer /data/archive/"$instrumentType"/"$run"
-        chmod -R 755 /data/archive/"$instrumentType"/"$run"
+	if [ $dragen -gt 0 ]
+	  then
 
-        #launch IlluminaQC for demultiplexing and QC
-        ssh transfer@10.59.210.245 "mkdir /data/archive/fastq/$run && cd /data/archive/fastq/$run && qsub -v sourceDir=/data/archive/$instrumentType/$run /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-$version/1_IlluminaQC.sh"
+	  echo "processing on Dragen"
+          #move to novaseq dir
+	  mv "$path" /data/raw/novaseq/ 
+
+        else
+
+
+          #log
+	  echo "path: $path"
+          echo "run: $run"
+          echo "instrumentType: $instrumentType"
+
+          #move run to archive
+          mv "$path" /data/archive/"$instrumentType"
+
+          #change access permissions
+          chown -R transfer /data/archive/"$instrumentType"/"$run"
+          chgrp -R transfer /data/archive/"$instrumentType"/"$run"
+          chmod -R 755 /data/archive/"$instrumentType"/"$run"
+
+          #launch IlluminaQC for demultiplexing and QC
+          ssh transfer@10.59.210.245 "mkdir /data/archive/fastq/$run && cd /data/archive/fastq/$run && qsub -v sourceDir=/data/archive/$instrumentType/$run /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-$version/1_IlluminaQC.sh"
+
+       fi
 
     done
+
 
 }
 
 processJobs "/data/raw/miseq"
 processJobs "/data/raw/hiseq"
 processJobs "/data/raw/nextseq"
-processJobs "/data/raw/novaseq"
