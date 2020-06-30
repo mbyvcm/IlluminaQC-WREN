0e8d3e0b2435cf38e57a583f84481bafc9fc8189
josephhalstead <josephhalstead89@gmail.com>
2020-06-30 15:00:35 +0100

    stop automatic processing of dragen pipelines

--- pipeline_cron.sh
+++ pipeline_cron.sh
@@ -7,68 +7,70 @@
 version="1.1.0"
 
 function processJobs {
-    echo "checking for jobs in $1 ..."
+	
+		echo "checking for jobs in $1 ..."
 
-    for path in $(find "$1" -maxdepth 2 -mindepth 2 -type f -name "RTAComplete.txt" -exec dirname '{}' \;); do
-        
+		for path in $(find "$1" -maxdepth 2 -mindepth 2 -type f -name "RTAComplete.txt" -exec dirname '{}' \;); do
+				
+				#extract run info from path
+				instrumentType=$(basename $(dirname "$path"))
+				run=$(basename "$path")
+				
+				#if the sample sheet exists
+				if [ -f "/data/raw/$instrumentType/$run/SampleSheet.csv" ]; then
+				
+					#check whether it has Dragen in it
+					set +u
+					dragen="$(grep Dragen /data/raw/"$instrumentType"/"$run"/SampleSheet.csv | wc -l)"
+					echo $dragen
+					set -u
 
-        #extract run info from path
-        instrumentType=$(basename $(dirname "$path"))
-        run=$(basename "$path")
-        
-        #if the sample sheet exists
-	if [ -f "/data/raw/$instrumentType/$run/SampleSheet.csv" ]; then
-        
-        #check whether it has Dragen in it
-        set +u
-	dragen="$(grep Dragen /data/raw/"$instrumentType"/"$run"/SampleSheet.csv | wc -l)"
-        echo $dragen
-        set -u
-        else
+				else
 
-        echo could not find sample sheet
+					echo could not find sample sheet
+					dragen=0
 
-        dragen=0
+				fi
 
-        fi
+				if [ $dragen -gt 0 ]; then
 
-	if [ $dragen -gt 0 ]
-	  then
+					echo "processing on Dragen"
 
-	  echo "processing on Dragen"
-          #move to novaseq dir
-	  mv "$path" /data/raw/novaseq/ 
-          #fix permissions          
-          chown -R sbsuser /data/raw/novaseq/"$run"
-          chgrp -R bi /data/raw/novaseq/"$run"
-          chmod -R 755 /data/raw/novaseq/"$run"
-          
+					#move to novaseq dir
+					mv "$path" /data/raw/novaseq/ 
 
-        else
+					touch /data/raw/novaseq/"$run"/dragen-detected
 
+					#fix permissions          
+					chown -R sbsuser /data/raw/novaseq/"$run"
+					chgrp -R bi /data/raw/novaseq/"$run"
+					chmod -R 755 /data/raw/novaseq/"$run"
+					
 
-          #log
-	  echo "path: $path"
-          echo "run: $run"
-          echo "instrumentType: $instrumentType"
+				else
 
-          #move run to archive
-          mv "$path" /data/archive/"$instrumentType"
-          
-          touch /data/archive/"$instrumentType"/"$run"/run_copy_complete.txt
+					#log
+					echo "path: $path"
+					echo "run: $run"
+					echo "instrumentType: $instrumentType"
 
-          #change access permissions
-          chown -R transfer /data/archive/"$instrumentType"/"$run"
-          chgrp -R transfer /data/archive/"$instrumentType"/"$run"
-          chmod -R 755 /data/archive/"$instrumentType"/"$run"
+					#move run to archive
+					mv "$path" /data/archive/"$instrumentType"
+					
+					# stop automatic processing
+					touch /data/archive/"$instrumentType"/"$run"/run_copy_complete.txt
 
-          #launch IlluminaQC for demultiplexing and QC
-          ssh transfer@10.59.210.245 "mkdir /data/archive/fastq/$run && cd /data/archive/fastq/$run && qsub -v sourceDir=/data/archive/$instrumentType/$run /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-$version/1_IlluminaQC.sh"
+					#change access permissions
+					chown -R transfer /data/archive/"$instrumentType"/"$run"
+					chgrp -R transfer /data/archive/"$instrumentType"/"$run"
+					chmod -R 755 /data/archive/"$instrumentType"/"$run"
 
-       fi
+					#launch IlluminaQC for demultiplexing and QC
+					ssh transfer@10.59.210.245 "mkdir /data/archive/fastq/$run && cd /data/archive/fastq/$run && qsub -v sourceDir=/data/archive/$instrumentType/$run /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-$version/1_IlluminaQC.sh"
 
-    done
+			 fi
 
+		done
 
 }
 
