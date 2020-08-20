cd9890ecb7c78e5c9a1cba6d902bfa24dafb00d4
Christopher Medway <christophermedway@gmail.com>
2020-08-20 09:27:34 +0100

    start SLURM/WREN refactor

--- pipeline_cron.sh
+++ pipeline_cron.sh
@@ -1,79 +1,46 @@
 #!/bin/sh
-#set -euo pipefail
+set -euo pipefail
 
-#Description: shell script to launch bioinformatics analysis pipelines. Run as root cron job without .sh extension
-#Author:Matt Lyon
-#Date: 17/09/19
-version="1.1.0"
+# Description: shell script to launch bioinformatics analysis pipelines. Run as root cron job without .sh extension
+# Date: 18/08/20
+version="1.2.0"
+
+bcl_raw_dir="/home/transfer/data/raw"
+bcl_arc_dir="/home/transfer/data/archive"
+fastq_dir="/home/transfer/data/archive/fastq"
+
 
 function processJobs {
-	
-		echo "checking for jobs in $1 ..."
+    echo "checking for jobs in $1 ..."
 
-		for path in $(find "$1" -maxdepth 2 -mindepth 2 -type f -name "RTAComplete.txt" -exec dirname '{}' \;); do
-				
-				#extract run info from path
-				instrumentType=$(basename $(dirname "$path"))
-				run=$(basename "$path")
-				
-				#if the sample sheet exists
-				if [ -f "/data/raw/$instrumentType/$run/SampleSheet.csv" ]; then
-				
-					#check whether it has Dragen in it
-					set +u
-					dragen="$(grep Dragen /data/raw/"$instrumentType"/"$run"/SampleSheet.csv | wc -l)"
-					echo $dragen
-					set -u
+    for path in $(find "$1" -maxdepth 2 -mindepth 2 -type f -name "RTAComplete.txt" -exec dirname '{}' \;); do
 
-				else
+        # extract run info from path
+        instrumentType=$(basename $(dirname "$path"))
+        run=$(basename "$path")
 
-					echo could not find sample sheet
-					dragen=0
+        # log
+	echo "path: $path"
+        echo "run: $run"
+        echo "instrumentType: $instrumentType"
 
-				fi
+        # move run to archive - CHANGE FROM CP
+        #cp -r "$path" "$bcl_arc_dir/$instrumentType/$run"
 
-				if [ $dragen -gt 0 ]; then
+        # change access permissions
+        #chown -R transfer "$bcl_arc_dir"/"$instrumentType"/"$run"
+        #chgrp -R transfer "$bcl_arc_dir"/"$instrumentType"/"$run"
+        #chmod -R 755 "$bcl_arc_dir"/"$instrumentType"/"$run"
 
-					echo "processing on Dragen"
+        # launch IlluminaQC for demultiplexing and QC
+        mkdir $fastq_dir/$run && cd $fastq_dir/$run
+        sbatch --export=sourceDir=$bcl_arc_dir/$instrumentType/$run /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-$version/1_IlluminaQC.sh
 
-					#move to novaseq dir
-					mv "$path" /data/raw/novaseq/ 
-
-					touch /data/raw/novaseq/"$run"/dragen-detected
-
-					#fix permissions          
-					chown -R sbsuser /data/raw/novaseq/"$run"
-					chgrp -R bi /data/raw/novaseq/"$run"
-					chmod -R 755 /data/raw/novaseq/"$run"
-					
-
-				else
-
-					#log
-					echo "path: $path"
-					echo "run: $run"
-					echo "instrumentType: $instrumentType"
-
-					#move run to archive
-					mv "$path" /data/archive/"$instrumentType"
-					
-					# stop automatic processing
-					touch /data/archive/"$instrumentType"/"$run"/run_copy_complete.txt
-
-					#change access permissions
-					chown -R transfer /data/archive/"$instrumentType"/"$run"
-					chgrp -R transfer /data/archive/"$instrumentType"/"$run"
-					chmod -R 755 /data/archive/"$instrumentType"/"$run"
-
-					#launch IlluminaQC for demultiplexing and QC
-					ssh transfer@10.59.210.245 "mkdir /data/archive/fastq/$run && cd /data/archive/fastq/$run && qsub -v sourceDir=/data/archive/$instrumentType/$run /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-$version/1_IlluminaQC.sh"
-
-			 fi
-
-		done
+    done
 
 }
 
-processJobs "/data/raw/miseq"
-processJobs "/data/raw/hiseq"
-processJobs "/data/raw/nextseq"
+processJobs "$bcl_raw_dir/hiseq"
+processJobs "$bcl_raw_dir/nextseq"
+processJobs "$bcl_raw_dir/novaseq"
+processJobs "$bcl_raw_dir/miseq"
