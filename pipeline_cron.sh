19781e73aae7438ff375cf5894d5ae42d2aba1b2
Christopher Medway <christophermedway@gmail.com>
2020-09-27 13:25:17 +0100

    changes to account to delay in sync between isilons

--- pipeline_cron.sh
+++ pipeline_cron.sh
@@ -6,14 +6,17 @@ set -euo pipefail
 # Date: 18/08/20
 version="1.2.0"
 
-# use to read raw data
-bcl_raw_read="/data/raw"
+# use to write raw data
+raw_write="/data_heath/raw"
+
+# where IlluminaQC reads data from
+raw_read="/data/raw"
 
 # use to write to archive 
-bcl_arc_write="/data_heath/archive"
+arc_write="/data_heath/archive"
 
 # use to read from archive
-bcl_arc_read="/data/archive"
+arc_read="/data/archive"
 
 # user to write from childnodes
 fastq_write="/Output/fastq"
@@ -33,21 +36,25 @@ function processJobs {
         echo "instrumentType: $instrumentType"
 
         # move run to archive
-        mv "$path" "$bcl_arc_write/$instrumentType/$run"
+        cp -r "$raw_write/$instrumentType/$run" "$arc_write/$instrumentType/$run"
 
         # change access permissions
-        chmod -R 755 "$bcl_arc_write"/"$instrumentType"/"$run"
-        chmod 777 "$bcl_arc_write"/"$instrumentType"/"$run"/SampleSheet.csv
+        chmod -R 755 "$arc_write"/"$instrumentType"/"$run"
+        chmod 777 "$arc_write"/"$instrumentType"/"$run"/SampleSheet.csv
 
-        # launch IlluminaQC for demultiplexing and QC
-        # currently, data is read from the Heath Isilon. Whilst not ideal, it may be preferable to a sleep while nodes sync 
-        ssh transfer@172.25.0.1 "mkdir $fastq_write/$run && cd $fastq_write/$run && sbatch -J IlluminaQC-"$run" --export=sourceDir=$bcl_arc_write/$instrumentType/$run /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-$version/1_IlluminaQC.sh"
+        # modify RTAComplete to prevent cron re-triggering
+        mv $raw_write/$instrumentType/$run/RTAComplete.txt $raw_write/$instrumentType/$run/_RTAComplete.txt
+
+        sleep 5m
+
+        # launch IlluminaQC for demultiplexing and QC 
+        ssh transfer@172.25.0.1 "mkdir $fastq_write/$run && cd $fastq_write/$run && sbatch -J IlluminaQC-"$run" --export=sourceDir=$path /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-$version/1_IlluminaQC.sh"
 
     done
 
 }
 
-#processJobs "$bcl_raw_dir/hiseq"
-processJobs "$bcl_raw_dir/nextseq"
-#processJobs "$bcl_raw_dir/novaseq"
-#processJobs "$bcl_raw_dir/miseq"
+#processJobs "$raw_read/hiseq"
+processJobs "$raw_read/nextseq"
+#processJobs "$raw_read/novaseq"
+#processJobs "$raw_read/miseq"
